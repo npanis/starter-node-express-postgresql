c355017a54312e1ac07f182ea0ea890cd7f76e13
Neriza Panis <neriza.montes@gmail.com>
2024-08-23 11:41:24 +0900

    CRUD with Knex

--- src/suppliers/suppliers.router.js
+++ src/suppliers/suppliers.router.js
@@ -2,10 +2,15 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./suppliers.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").post(controller.create).all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 router
-  .route("/:supplierId")
+  .route("/:supplierId([0-9]+)")
+  .get(controller.read)
   .put(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
