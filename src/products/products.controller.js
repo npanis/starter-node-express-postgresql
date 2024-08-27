ccd00ef22910dc71884c87a9a938853a31ba243c
Neriza Panis <neriza.montes@gmail.com>
2024-08-27 12:37:00 +0900

    Joins with Knex

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -20,8 +20,23 @@ function read(req, res) {
   res.json({ data });
 }
 
+async function listOutOfStockCount(req, res, next){
+  res.json({ data: await productsService.listOutOfStockCount() });
+}
+
+async function listPriceSummary(req, res, next) {
+  res.json({ data: await productsService.listPriceSummary() });
+}
+
+async function listTotalWeightByProduct(req, res) {
+  res.json({ data: await productsService.listTotalWeightByProduct() });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
+  listOutOfStockCount: asyncErrorBoundary(listOutOfStockCount),
+  listPriceSummary: asyncErrorBoundary(listPriceSummary),
+  listTotalWeightByProduct: asyncErrorBoundary(listTotalWeightByProduct),
   read : [
     asyncErrorBoundary(productExists),
     read
